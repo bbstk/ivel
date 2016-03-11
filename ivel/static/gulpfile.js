9dc39263fc51163402ea0fee74fb7cb2662ca692
stelaseldano <stella.plamenova@gmail.com>
2016-03-11 15:52:18 +0100

    static added

---
+++ ivel/static/gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+
+
+// handle dependencies
+gulp.task('dependencies', function() {
+	gulp.src('node_modules/normalize.css/normalize.css')
+		.pipe(gulp.dest('export'));
+});
+
+
+// process css
+var stylus = require('gulp-stylus');
+
+gulp.task('stylus', function() {
+	gulp.src('source/*.styl')
+		.pipe(stylus())
+		.pipe(gulp.dest('export'));
+});
+
+
+// what to watch for
+gulp.task('watch', function() {
+    gulp.watch('source/*.styl', ['stylus']);
+});
+
+
+// the default task
+gulp.task('default', function() {
+	gulp.start([
+		'dependencies',
+        'stylus',
+		'watch'
+	]);
+});
